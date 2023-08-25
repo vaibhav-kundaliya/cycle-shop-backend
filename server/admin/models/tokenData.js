ed1a0471b3b64129da02b812a2b87e82571e1031
vaibhav-kundaliya <141109310+vaibhav-kundaliya@users.noreply.github.com>
2023-08-25 15:58:42 +0530

    admin part removed

--- server/admin/models/tokenData.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const tokenSchema = new mongoose.Schema({
-  token: { type: String },
-  email: { type: String, require: true },
-});
-const tokenData = mongoose.model("tokenData", tokenSchema);
-module.exports = tokenData;
